e57fd9d630709fa919519ac66eab4e898bc17fc5
zgtcgd <132292985+zgtcgd@users.noreply.github.com>
2023-12-30 18:01:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const FILE_PATH = process.env.FILE_PATH || '/tmp/';
 const express = require("express");
 const app = express();
 const { createProxyMiddleware } = require("http-proxy-middleware");
+var fs = require("fs");
 var exec = require("child_process").exec;
 
 app.get("/", function (req, res) {
@@ -15,25 +16,26 @@ app.get("/healthcheck", function (req, res) {
 
 //获取节点数据
 app.get("/list", function (req, res) {
-    let cmdStr = "cat " + FILE_PATH + "list.txt";
-    exec(cmdStr, function (err, stdout, stderr) {
-      if (err) {
-        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-      }
-      else {
-        res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
-      }
-    });
+  let filePath = FILE_PATH + "list.txt";
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.type("html").send("<pre>文件读取错误：\n" + err + "</pre>");
+    }
+    else {
+      res.type("html").send("<pre>节点数据：\n\n" + data + "</pre>");
+    }
   });
+});
 
 //获取订阅数据
 app.get("/sub", (req, res) => {
-    let cmdStr = "cat " + FILE_PATH + "sub.txt";
-    exec(cmdStr, function (err, stdout, stderr) {
+  let subfilePath = FILE_PATH + "sub.txt";
+  fs.readFile(subfilePath, (err, data) => {
     if (err) {
       res.status(500).send('Error reading file');
-    } else {
-      res.type("txt").send(escape(stdout));
+    }
+    else {
+      res.type("txt").send(data);
     }
   });
 });
