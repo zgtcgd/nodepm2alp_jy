eec850c7e59540f68a8b4d21ab86ee79ecdd82e7
zgtcgd <132292985+zgtcgd@users.noreply.github.com>
2024-01-16 20:40:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,57 @@
 const port = process.env.PORT || 3000;
 const FILE_PATH = process.env.FILE_PATH || '/tmp';
-const express = require("express");
-const app = express();
-const { createProxyMiddleware } = require("http-proxy-middleware");
-var fs = require("fs");
-const { spawn } = require('child_process');
+const http = require('http');
+const fs = require('fs');
+var exec = require("child_process").exec;
 
-// 启动主程序
-const startScriptPath = `/app/start.sh`;
-const childProcess = spawn(startScriptPath, [], {
-  detached: false,
-  stdio: 'inherit',
+const listFilePath = FILE_PATH + '/list.txt';
+const subFilePath = FILE_PATH + '/sub.txt';
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200);
+    res.end('hello world');
+
+  } else if (req.url === '/healthcheck') {
+    res.writeHead(200);
+    res.end('ok');
+
+  } else if (req.url === '/list') {
+    fs.readFile(listFilePath, 'utf8', (error, data) => {
+      if (error) {
+        res.writeHead(500);
+        res.end('Error reading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end(data);
+      }
+    });
+
+  } else if (req.url === '/sub') {
+    fs.readFile(subFilePath, 'utf8', (error, data) => {
+      if (error) {
+        res.writeHead(500);
+        res.end('Error reading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
 });
 
-app.get("/", function (req, res) {
-  res.status(200).send("hello world");
+//启动主程序
+exec("bash /app/start.sh", function (err, stdout, stderr) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
 });
 
-app.get("/healthcheck", function (req, res) {
-  res.status(200).send("ok");
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-//获取节点数据
-app.get("/list", function (req, res) {
-  let filePath = FILE_PATH + "/list.txt";
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.type("html").send("<pre>文件读取错误：\n" + err + "</pre>");
-    }
-    else {
-      res.type("html").send("<pre>节点数据：\n\n" + data + "</pre>");
-    }
-  });
-});
-
-//获取订阅数据
-app.get("/sub", (req, res) => {
-  let subfilePath = FILE_PATH + "/sub.txt";
-  fs.readFile(subfilePath, (err, data) => {
-    if (err) {
-      res.status(500).send('Error reading file');
-    }
-    else {
-      res.type("txt").send(data);
-    }
-  });
-});
-
-//获取系统进程表
-app.get("/status", function (req, res) {
-  let cmdStr = "ps -ef";
-  exec(cmdStr, function (err, stdout, stderr) {
-    if (err) {
-      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-    }
-    else {
-      res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
-    }
-  });
-});
-
-app.use(
-  "/",
-  createProxyMiddleware({
-    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
-    onProxyReq: function onProxyReq(proxyReq, req, res) {},
-    pathRewrite: {
-      // 请求中去除/
-      "^/": "/"
-    },
-    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
-    ws: true // 是否代理websockets
-  })
-);
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
