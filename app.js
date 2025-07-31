7c75166a6adca8dab4383e62a28214203afdb824
zgtcgd <132292985+zgtcgd@users.noreply.github.com>
2025-07-31 22:20:56 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 const port = process.env.PORT || 3000;
 const FILE_PATH = process.env.FILE_PATH || '/tmp';
+const UUID = process.env.UUID;
 const express = require("express");
 const app = express();
 const fs = require("fs");
 const { spawn } = require('child_process');
-const openserver = process.env.openserver || '1';
+const OPENSERVER = process.env.OPENSERVER || '1';
 
 app.get("/", function (req, res) {
   res.status(200).send("hello world");
 });
 
-app.get("/sub", (req, res) => {
-  let subfilePath = FILE_PATH + "/log.txt";
+app.get(`/${UUID}`, (req, res) => {
+  let subfilePath = path.join(FILE_PATH, "log.txt");
   fs.readFile(subfilePath, (err, data) => {
     if (err) {
-      res.status(500).send('Error reading file');
+      return res.status(500).send('Error reading file');
     }
-    else {
-      res.type("txt").send(data);
-    }
+    res.type("txt").send(data);
   });
 });
 
-if (openserver === '1') {
+if (OPENSERVER === '1') {
   // do nothing
-} else if (openserver === '0') {
+} else if (OPENSERVER === '0') {
   const { createProxyMiddleware } = require("http-proxy-middleware");
   app.use(
     "/",
